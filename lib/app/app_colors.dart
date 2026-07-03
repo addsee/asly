c1921e5cab56e69a61b1e9bdeeb08907a1e452b8
Mo Suliman <addsee2@gmail.com>
2026-07-03 17:06:33 +0100

    initial commit

--- lib/app/app_colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/animation.dart';
-
-class AppColors {
-  static Color main = const Color(0xff1B395B); 
-}
--- lib/app/app_routes.dart
+++
@@ -1,6 +0,0 @@
-class AppRoutes {
-  static final String root = "/root";
-  static final String home = "/home";
-  static final String ownership = "/ownership";
-  static final String addnew = "/addnew";
-}
